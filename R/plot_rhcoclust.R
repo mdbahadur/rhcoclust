0df07643523e1cdd55a74f9cecd094caba9506ee
badsha <mdbadsha@uidaho.edu>
2020-05-03 14:15:30 -0700

    Updated plot arguments for cocluster and ccim

--- R/plot_rhcoclust.R
+++ R/plot_rhcoclust.R
@@ -1,16 +1,15 @@
 #' @export
-plot_rhcoclust <- function(CoClustObj,plot.cocluster=FALSE,plot.ccim=FALSE){
+plot_rhcoclust <- function(CoClustObj, plot.cocluster = FALSE, plot.ccim = FALSE){
 
 # Plot results for gene (row) and compound (column) co-cluster graph
-par(mar=c(4,7,1,3))
-#par(mfrow=c(1,2))
+
 # Transformed data matrix (rearranged by RHCOC algorithm) to generate co-cluster graph.
 CoClustData <- CoClustObj$CoClsDtMat
 
 # Row and column cluster and their co-cluster mean.
 Coclust_MeanMat <- CoClustObj$Coclust_MeanMat
 
-# Shape of points to generate control chart for individual measurement.
+# Shape of points to generate control chart for individual measurement (CCIM).
 PcmQC <- CoClustObj$pchmark
 
 # Colors to generate control chart for individual measurement.
@@ -35,45 +34,60 @@ UCL_QC <- CoClustObj$UpContLimit
 LCL_QC <- CoClustObj$LowrContLimit
 
 # Display a color image for co-cluster
-if (plot.cocluster)
-{image(CoClustData,
-      col = colorRampPalette(c("white","gray","black"),space = "rgb")(300),
-      axes = FALSE,
-      main="Co-cluster graph")
-
+if (plot.cocluster==TRUE)
+{
+image(CoClustData,
+        col = colorRampPalette(c("white","gray","black"),space = "rgb")(300),
+        axes = FALSE)
 # Show row names for the image
 mtext(text = rownames(CoClustData),
-      side = 1,
-      line = 0.3,
-      at = seq(0,1,1/(nrow(CoClustData)-1)),
-      las = 2,
-      cex = 0.6,
-      col = colors.genes)
-
+        side = 1,
+        line = 0.3,
+        at = seq(0,1,1/(nrow(CoClustData)-1)),
+        las = 2,
+        cex = 0.6,
+        col = colors.genes)
 # Show column names for the image
 mtext(text = colnames(CoClustData),
-      side = 2,
-      line = 0.3,
-      at = seq(0,1,1/(ncol(CoClustData)-1)),
-      las = 2,
-      cex = 0.6,
-      col = colors.dcc)
-
+        side = 2,
+        line = 0.4,
+        at = seq(0,1,1/(ncol(CoClustData)-1)),
+        las = 2,
+        cex = 0.4,
+        col = colors.dcc)
+# x-axis
+mtext("Row clusters",
+        side = 1,
+        line = 3.5,
+        cex = 1.5)
+# y-axis
+mtext("Column clusters",
+        side = 2,
+        line = 5.5,
+        cex = 1.5)
+# Title
+title("Co-cluster graph",
+        adj = 0.5,
+        line = 1,
+        cex.main = 1.5)
 # Show a legend strip for the color scale
 image.plot(CoClustData,
-           col = colorRampPalette(c("white","gray","black"),space = "rgb")(300),
-           legend.only = TRUE,
-           add = TRUE,
-           xpd = TRUE,
-           legend.shrink=1,
-           horizontal = FALSE)
+             col = colorRampPalette(c("white","gray","black"),space = "rgb")(300),
+             legend.only = TRUE,
+             add = TRUE,
+             xpd = TRUE,
+             legend.shrink = 1,
+             legend.mar = 1.5,
+             horizontal = FALSE)
 }
 # Plot graph of control chart for individual measurement for identification of biomarker co-clusters.
-if (plot.ccim)
-{plot(x = Coclust_MeanMat[,2],
-     xlab = "Combination of Row and Column Cluster",
-     ylab = "Co-cluster Average",
-     main = "Graph for QCC",
+if (plot.ccim==TRUE)
+{
+#par(mar=c(6,10,3,6)) # Modify if needed
+# mar order: bottom, left, top, and right
+plot(x = Coclust_MeanMat[,2],
+     xlab = "",
+     ylab = "",
      xaxt = 'n',
      pch = PcmQC,
      cex = 1.1,
@@ -81,9 +95,7 @@ if (plot.ccim)
      ylim = c(min(LCL_QC,min(Coclust_MeanMat[,2]-5)),max(Coclust_MeanMat[,2]+2)))
 
 # Add straight lines to a plot
-abline(h = c(UCL_QC,
-             CntrLine_QC,
-             LCL_QC),
+abline(h = c(UCL_QC,CntrLine_QC,LCL_QC),
        lty = 3,
        lwd = 3,
        col = "green")
@@ -95,11 +107,25 @@ mtext(text = Coclust_MeanMat[,1],
       las = 2,
       font = 2,
       cex = 1,
-      col = "black")
+        col = "black")
+# x-axis
+mtext("Combination of Row and Column Cluster",
+      side = 1,
+      line = 2,
+      cex = 1.2)
+# y-axis
+mtext("Co-cluster Average",
+      side = 2,
+      line = 2.5,
+      cex = 1.2)
+# Title
+title("Graph for CCIM",
+      adj = 0.5,
+      line = 0.5)
 
 # Add UCL,CL,LCL to a plot
 text(c(length(Coclust_MeanMat[,1])-0.5,length(Coclust_MeanMat[,1])-0.5,
-       length(Coclust_MeanMat[,1])-0.5),
+         length(Coclust_MeanMat[,1])-0.5),
      c(UCL_QC, CntrLine_QC, LCL_QC),
      c("UCL","CL","LCL"),
      font = 4,
